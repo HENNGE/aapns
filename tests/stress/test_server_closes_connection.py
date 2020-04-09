dc50b0148e854e5db5c54f600c234112ad2a5d60
Dima Tisnek <dimaqq@gmail.com>
2020-04-09 13:44:17 +0900

    kill go run zombies

--- tests/stress/test_server_closes_connection.py
+++ tests/stress/test_server_closes_connection.py
@@ -3,24 +3,38 @@
     * Closed('[Errno 54] Connection reset by peer') 
     * Closed('Server closed the connection')
 """
+from os import killpg
+from signal import SIGTERM
 from asyncio import create_subprocess_exec
 from asyncio.subprocess import PIPE
+from contextlib import suppress
 import logging
 
-async def test_1():
-    x = await create_subprocess_exec("go", "run", "tests/stress/server-ok.go", stdout=PIPE, stderr=PIPE)
-    w = logging.warning
-    w("%s", x)
-    w("%s", x.stdout)
 
-    async def rr(what, name):
+async def rr(what, name):
+    with suppress(CancelledError):
         async for line in what:
-            w("%s: %s", name, line.decode("utf-8"))
+            logging.warning("%s: %s", name, line.decode("utf-8"))
 
-    to = asyncio.create_task(rr(x.stdout, "server:stdout"))
-    to = asyncio.create_task(rr(x.stderr, "server:stderr"))
 
-    await asyncio.sleep(5)
+async def test_1():
+    server = await create_subprocess_exec(
+        "go", "run", "tests/stress/server-ok.go", stdout=PIPE, stderr=PIPE, start_new_session=True
+    )
+    logging.warning("server %r", server)
+    try:
+        to = asyncio.create_task(rr(server.stdout, "server:stdout"))
+        te = asyncio.create_task(rr(server.stderr, "server:stderr"))
+        try:
+            await asyncio.sleep(5)
+        finally:
+            to.cancel()
+            te.cancel()
+    finally:
+        with suppress(ProcessLookupError):
+            # server.terminate() is not enough,`go run`'s child somehow survives
+            # Thus, I'm starting the server in a new session and kill the entire session
+            killpg(server.pid, SIGTERM)
 
 
 if __name__ == "__main__":
